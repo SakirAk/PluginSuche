e9cf1fd639a235be3199a7190e7c4cb080f36a66
SakirAk <36919355+SakirAk@users.noreply.github.com>
2018-03-06 23:52:44 +0100

    Update plz-script.js
    switch durch if and else ersetz

--- plz-script.js
+++ plz-script.js
@@ -5,83 +5,91 @@ console.log("javascript wird angesprochen");
     // Get the value of the input field with id="numb"
     var inputValue = document.getElementById("plz").value;
 	console.log(inputValue);	
-	
-	switch (inputValue) {
-		case inputValue => 1000 && inputValue < 7000:
-			  if(inputValue){
-			  text = "UUHGuhuiddddhuhh";
-			  document.getElementById("demo").innerHTML = text;}
-			  else
-			break;
-		case inputValue => 7000 && inputValue < 9000:
-			  text = " x zwischen 7000 und 9000 ";
-			break;
-		case inputValue => 8000 && inputValue < 14000:
-			  text = " x zwischen 8000 und 14000 ";
-			break;
-		case inputValue => 14000 && inputValue < 15000:
-			  text = " x zwischen 14000 und 15000 ";
-			break;
-		case inputValue => 15000&& inputValue < 17000:
-			  text = " x zwischen 15000 und 17000 ";
-			break;
-		case inputValue => 17000&& inputValue < 20000:
-			  text = " x zwischen 17000 und 20000 ";
-			break;
-		case inputValue => 20000&& inputValue < 23000:
-			  text = " x zwischen 20000 und 23000 ";
-			break;
-		case inputValue => 23000&& inputValue < 240000:
-			  text = " x zwischen 23000 und 24000 ";
-			break;
-	   case inputValue => 24000&& inputValue < 29000:
-			  text = " x zwischen 24000 und 29000 ";
-			break;
-		case inputValue => 29000&& inputValue < 33000:
-			  text = " x zwischen 29000 und 33000 ";
-			break;	
-		case inputValue => 33000&& inputValue < 34000:
-			  text = " x zwischen 33000 und 34000 ";
-			break;
-		case inputValue => 34000&& inputValue < 35000:
-			  text = " x zwischen 34000 und 35000 ";
-			break;
-		case inputValue => 35000&& inputValue < 36000:
-			  text = " x zwischen 35000 und 36000 ";
-			break;
-		case inputValue => 36000&& inputValue < 38000:
-			  text = " x zwischen 36000 und 38000 ";
-			break;
-		case inputValue => 38000&& inputValue < 40000:
-			  text = " x zwischen 38000 und 40000 ";
-			break;
-		case inputValue => 40000&& inputValue < 48000:
-			  text = " x zwischen 38000 und 48000 ";
-			break;
-		case inputValue => 48000&& inputValuex < 50000:
-			  text = " x zwischen 48000 und 50000 ";
-			break;
-		case inputValue => 50000&& inputValue < 63000:
-			  text = " x zwischen 50000 und 63000 ";
-			break;
-		case inputValue => 63000&& inputValue < 66000:
-			  text = " x zwischen 63000 und 660000 ";
-			break;
-		case inputValue => 66000&& inputValue < 80000:
-			  text = " x zwischen 66000 und 80000 ";
-			break;
-		case inputValue => 80000&& inputValue < 99000:
-			  text = " x zwischen 80000 und 990000 ";
-			break;
-		case inputValue => 99000&& inputValue < 1000000:
-			  text = " x zwischen 66000 und 1000000 ";
-			break;
-			
-		default: 
-			text =" Bitte geben Sie eine valide Postleitzahl ein.";
-			break;
-      
-		console.log("ICH BIN DURCH");
+
+if(inputValue>1000  && inputValue < 7000 ){
+    text = "Name: David Clasen email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>7000  && inputValue < 9000 ){
+    text = "Name: Andreas Ruschepaul email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>8000  && inputValue < 14000 ){
+    text = "Name: David Clasen email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>1400  && inputValue < 15000 ){
+    text = "Name: Cristian Clasen email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>15000  && inputValue < 17000 ){
+    text = "Name: David Clasen email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>17000  && inputValue < 20000 ){
+    text = "Name: Christian Clasen email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>20000  && inputValue < 23000 ){
+    text = "Name: Marien Dudeck email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>23000  && inputValue < 24000 ){
+    text = "Name: Christian Clasen email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>24000  && inputValue < 29000 ){
+    text = "Name: Marien Dudeck email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>29000  && inputValue < 33000 ){
+    text = "Name: Christian Clasen email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>33000  && inputValue < 35000 ){
+    text = "Name: Marien Dudeck email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>35000  && inputValue < 36000 ){
+    text = "Name: Andreas Ruschepaul email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>36000  && inputValue < 38000 ){
+    text = "Name: Egon Lahoff email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>38000  && inputValue < 40000 ){
+    text = "Name: Andreas Ruschepaul email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>40000  && inputValue < 48000 ){
+    text = "Name: Egon Lahoff email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>48000  && inputValue < 50000 ){
+    text = "Name: Marien Dudeck email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>50000  && inputValue < 63000 ){
+    text = "Name: Egon Lahoff email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>63000  && inputValue < 66000 ){
+    text = "Name: Christian Moll email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>66000  && inputValue < 80000 ){
+    text = "Name: Jürgen Lager email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>80000  && inputValue < 99000 ){
+    text = "Name: Christian Moll email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
+else if(inputValue>99000  && inputValue < 1000000 ){
+    text = "Name: Andreas Ruschepaul email: fafq@fakljfaja.de nummer:237462";
+    document.getElementById("demo").innerHTML = text;
+}
   
     //document.getElementById("demo").innerHTML = text;
 	}
